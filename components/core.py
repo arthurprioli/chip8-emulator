78487ff27b7f84cfda2e0237268565fbbcddb1db
Arthur Reigada Prioli Menezes <arthur.prioli@gmail.com>
2026-06-04 13:19:23 -0300

    feat: adds IO and cpu pointer allocation

--- components/core.py
+++ components/core.py
@@ -1,15 +1,44 @@
 """
-    Integrates all components to simulate an instruction cycle.
+Integrates all components to simulate an instruction cycle.
 """
+
 from cpu import CPU
 from memory import Memory
 from display import Display
 from helpers.timers import Timers
+from typing import List
+from ctypes import c_uint16
+
 
 class Core:
     def __init__(self):
-        cpu = CPU()
-        memory = Memory()
+        """
+        Starts the core of the emulator, defines all parameters to its standards.
+        """
+        self.cpu = CPU()
+        self.memory = Memory()
+        instructions: List[c_uint16] = [
+            0x00E0,
+            0x1000,
+            0x6000,
+            0x7000,
+            0xA000,
+            0xD000,
+        ]
 
-        # point index pointer to beggining of memory
-        
+        # load instruction to first avaliable memory address in IO
+        self.memory.load_instructions(instructions)
+        self.cpu.pc = 0xFF + 1  # hard-coded - first address with instructions
+
+    def fetch_instruction(self):
+        """
+        Fetches the instruction on the memory.
+        """
+        instruction = self.memory.curr_memory[self.cpu.pc]
+        self.cpu.pc += 1
+        return instruction
+
+
+if __name__ == "__main__":
+    core = Core()
+    print(core.fetch_instruction())
--- components/memory.py
+++ components/memory.py
@@ -1,14 +1,24 @@
 from ctypes import c_uint16, c_uint8
 from typing import List
-from .helpers.font import font
+from helpers.font import font
+
 
 class Memory:
     """
-        Defines our memory class.
+    Defines our memory class.
     """
+
     def __init__(self):
-        self.curr_memory : List[c_uint16] = [0x0] * 4096
-        self.stack : List[c_uint16] = [0x0, 0x0]
+        self.curr_memory: List[c_uint16] = [0x0] * 4096
+        self.stack: List[c_uint16] = [0x0, 0x0]
 
         self.curr_memory[0x00:0xFF] = font
-    
+
+    def load_instructions(self, instructions: List[c_uint16]):
+        """
+        Loads instructions in the first avaliable memory address.
+
+        Args:
+            instructions: The list of instructions to place in memory.
+        """
+        self.curr_memory[0xFF:] = instructions
