a6445205c4d2c8b6b51789b2c18f58ead8f2a919
Arthur Reigada Prioli Menezes <arthur.prioli@gmail.com>
2026-06-06 16:35:54 -0300

    feat: added instructions, need debugging

--- components/core.py
+++ components/core.py
@@ -2,17 +2,18 @@
 Integrates all components to simulate an instruction cycle.
 """
 
-import sdl3
+import random
 
 from components.cpu import CPU
 from components.memory import Memory
 from components.display import Display
 from components.rom_reader import RomReader
 from components.helpers.timers import Timers
+from components.input_handler import InputHandler
 from typing import List
 from ctypes import c_uint16
 
-ROM_PATH = "/home/arthurprioli/Documentos/dev/fun/chip8-emulator/roms/IBM Logo.ch8"
+ROM_PATH = "/home/arthurprioli/Documentos/dev/fun/chip8-emulator/roms/bc_test.ch8"
 
 
 class Core:
@@ -28,6 +29,8 @@ class Core:
         # load instruction to first avaliable memory address in IO
         self.memory.load_instructions(self.instructions)
         self.cpu.pc = 0x200  # hard-coded - first address with instructions
+        self.input_handler = InputHandler()
+        self.timers = Timers()
 
     def fetch_instruction(self):
         """
@@ -89,6 +92,9 @@ class Core:
                     self.cpu.pc += 2
             case 0x4:
                 to_compare = (third_nibble << 4) | fourth_nibble
+                print(
+                    f"Comparing {hex(to_compare)} with {hex(self.cpu.registers[second_nibble])}"
+                )
                 if self.cpu.registers[second_nibble] != to_compare:
                     self.cpu.pc += 2
             case 0x5:
@@ -104,6 +110,84 @@ class Core:
             case 0x7:
                 new_val = (third_nibble << 4) | fourth_nibble
                 self.cpu.registers[second_nibble] += new_val
+                print(f"Setting register v{second_nibble} to {new_val}...")
+            case 0x8:
+                match fourth_nibble:
+                    case 0x0:
+                        self.cpu.registers[second_nibble] = self.cpu.registers[
+                            third_nibble
+                        ]
+                    case 0x1:
+                        self.cpu.registers[second_nibble] |= self.cpu.registers[
+                            third_nibble
+                        ]
+                    case 0x2:
+                        self.cpu.registers[second_nibble] &= self.cpu.registers[
+                            third_nibble
+                        ]
+                    case 0x3:
+                        self.cpu.registers[second_nibble] ^= self.cpu.registers[
+                            third_nibble
+                        ]
+                    case 0x4:
+                        self.cpu.registers[second_nibble] += self.cpu.registers[
+                            third_nibble
+                        ]
+                        if self.cpu.registers[second_nibble] > 0xFF:
+                            self.cpu.registers[0xF] = 1
+                        else:
+                            self.cpu.registers[0xF] = 0
+
+                        self.cpu.registers[second_nibble] = (
+                            self.cpu.registers[second_nibble] % 0xFF
+                        )
+                    case 0x5:
+                        if (
+                            self.cpu.registers[second_nibble]
+                            > self.cpu.registers[third_nibble]
+                        ):
+                            self.cpu.registers[0xF] = 1
+                        else:
+                            self.cpu.registers[0xF] = 0
+
+                        self.cpu.registers[second_nibble] -= self.cpu.registers[
+                            third_nibble
+                        ]
+
+                    case 0x7:
+                        if (
+                            self.cpu.registers[third_nibble]
+                            > self.cpu.registers[second_nibble]
+                        ):
+                            self.cpu.registers[0xF] = 1
+                        else:
+                            self.cpu.registers[0xF] = 0
+
+                        self.cpu.registers[third_nibble] -= self.cpu.registers[
+                            second_nibble
+                        ]
+            case 0x8:
+                match fourth_nibble:
+                    case 0x6:
+                        self.cpu.registers[second_nibble] = self.cpu.registers[
+                            third_nibble
+                        ]
+                        if self.cpu.registers[second_nibble] & 0x01:
+                            self.cpu.registers[0xF] = 1
+                        else:
+                            self.cpu.registers[0xF] = 0
+
+                        self.cpu.registers[second_nibble] <<= 1
+                    case 0xE:
+                        self.cpu.registers[second_nibble] = self.cpu.registers[
+                            third_nibble
+                        ]
+                        if self.cpu.registers[second_nibble] & 0x80:
+                            self.cpu.registers[0xF] = 1
+                        else:
+                            self.cpu.registers[0xF] = 0
+
+                        self.cpu.registers[second_nibble] >>= 1
             case 0x9:
                 if (
                     self.cpu.registers[second_nibble]
@@ -116,6 +200,21 @@ class Core:
                 )
                 print(f"Setting index pointer to {hex(destination_addr)}...")
                 self.cpu.index = destination_addr
+            case 0xB:
+                destination_addr = (
+                    (second_nibble << 8) | (third_nibble << 4) | fourth_nibble
+                )
+                final_addr = destination_addr + self.cpu.registers[0]
+                self.cpu.pc = print(
+                    f"Jumping to instruction in address {hex(final_addr)}"
+                )
+            case 0xC:
+                random_num = random.randbytes(1)
+                final_val = random_num & ((third_nibble << 4) | fourth_nibble)
+                self.cpu.registers[second_nibble] = final_val
+                print(
+                    f"Generating random number {random_num}, ANDing it to {final_val}  and putting into v{second_nibble}"
+                )
             case 0xD:
                 x_coord = self.cpu.registers[second_nibble] % 64
                 y_coord = self.cpu.registers[third_nibble] % 32
@@ -123,6 +222,7 @@ class Core:
                 self.cpu.registers[0xF] = 0
                 for i in range(rows):
                     sprite_data = self.memory.curr_memory[self.cpu.index + i]
+                    print(f"Current sprite data: f{sprite_data}")
                     for j in range(8):
                         target_y = y_coord + i
                         target_x = x_coord + j
@@ -138,6 +238,66 @@ class Core:
                                 self.cpu.registers[0xF] = 1
 
                             self.display.frame_buffer[index_in_display] ^= 1
+            case 0xE:
+                match third_nibble:
+                    case 0x9:
+                        if self.input_handler.check_key_pressed(second_nibble):
+                            self.cpu.pc += 2
+                    case 0xA:
+                        if not self.input_handler.check_key_pressed(second_nibble):
+                            self.cpu.pc += 2
+            case 0xF:
+                match third_nibble:
+                    case 0x0:
+                        match fourth_nibble:
+                            case 0x7:
+                                self.cpu.registers[second_nibble] = (
+                                    self.timers.delay_timer
+                                )
+                            case 0xA:
+                                self.cpu.registers[second_nibble] = (
+                                    self.input_handler.await_input()
+                                )
+                    case 0x1:
+                        match fourth_nibble:
+                            case 0x5:
+                                self.times.delay_timer = self.cpu.registers[
+                                    second_nibble
+                                ]
+                            case 0x8:
+                                self.times.sound_timer = self.cpu.registers[
+                                    second_nibble
+                                ]
+                            case 0xE:
+                                self.cpu.index += self.cpu.registers[second_nibble]
+                                if (self.cpu.index > 0x1000) or (
+                                    self.cpu.index < 0x0FFF
+                                ):
+                                    self.cpu.registers[0xF] = 1
+                    case 0x2:
+                        self.cpu.index = self.memory.curr_memory[second_nibble * 5]
+                    case 0x3:
+                        first_num, second_num, third_num = (
+                            self.cpu.registers[second_nibble] // 100,
+                            (self.cpu.registers[second_nibble] // 10) % 10,
+                            self.cpu.registers[second_nibble] % 100,
+                        )
+                        self.memory.curr_memory[self.cpu.index] = first_num
+                        self.memory.curr_memory[self.cpu.index + 1] = second_num
+                        self.memory.curr_memory[self.cpu.index + 2] = third_num
+                    case 0x5:
+                        curr_addr = self.cpu.index
+                        for i in range(second_nibble):
+                            self.memory.curr_memory[curr_addr + i] = self.cpu.registers[
+                                i
+                            ]
+                    case 0x6:
+                        curr_addr = self.cpu.index
+                        for i in range(second_nibble):
+                            self.cpu.registers[i] = self.memory.curr_memory[
+                                curr_addr + i
+                            ]
+
 
 if __name__ == "__main__":
     core = Core()
---
+++ components/input_handler.py
@@ -0,0 +1,48 @@
+import sdl3
+
+
+class InputHandler:
+    """
+    Class responsible for treating SDL Keyboard inputs
+    """
+
+    def _handle_key(self, scancode):
+        if scancode in (
+            sdl3.SDL_SCANCODE_1,
+            sdl3.SDL_SCANCODE_2,
+            sdl3.SDL_SCANCODE_3,
+            sdl3.SDL_SCANCODE_4,
+            sdl3.SDL_SCANCODE_5,
+            sdl3.SDL_SCANCODE_6,
+            sdl3.SDL_SCANCODE_7,
+            sdl3.SDL_SCANCODE_8,
+            sdl3.SDL_SCANCODE_9,
+        ):
+            return scancode - 29
+        elif scancode == sdl3.SDL_SCANCODE_0:
+            return 0
+        elif scancode == sdl3.SDL_SCANCODE_Q:
+            return 0xA
+        elif scancode == sdl3.SDL_SCANCODE_W:
+            return 0xB
+        elif scancode == sdl3.SDL_SCANCODE_E:
+            return 0xC
+        elif scancode == sdl3.SDL_SCANCODE_R:
+            return 0xD
+        elif scancode == sdl3.SDL_SCANCODE_T:
+            return 0xE
+        elif scancode == sdl3.SDL_SCANCODE_Y:
+            return 0xF
+
+    def await_key(self):
+        event = sdl3.SDL_Event()
+        while True:
+            sdl3.SDL_WaitEvent(event)
+            if event.type == sdl3.SDL_EVENT_KEY_DOWN:
+                return self._handle_key(event.key.scancode)
+
+    def check_key_pressed(self, value):
+        event = sdl3.SDL_Event()
+        while sdl3.SDL_PollEvent(event):
+            if event.type == sdl3.SDL_EVENT_KEY_DOWN:
+                return self._handle_key(event.key.scancode)
