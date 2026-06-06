28b5f9b7c229d7684abd622c233416eb6064bb09
Arthur Prioli <arthur.prioli@gmail.com>
2026-06-05 23:28:30 -0300

    feat: starts decoding instructions (todo: implement bit drawing)

--- components/core.py
+++ components/core.py
@@ -19,7 +19,7 @@ class Core:
         self.memory = Memory()
         instructions: List[c_uint16] = [
             0x00E0,
-            0x1000,
+            0x1234,
             0x6000,
             0x7000,
             0xA000,
@@ -38,7 +38,71 @@ class Core:
         self.cpu.pc += 1
         return instruction
 
+    def decode_instruction(self, instruction):
+        """
+        Decodes the fetched instruction.
+
+        0001 0010 0011 0100 
+        """
+        first_nibble = (instruction >> 12) # 0001
+        second_nibble = (instruction >> 8) & 0x0F # 0010
+        third_nibble = (instruction >> 4) & 0x00F # 0011
+        fourth_nibble = instruction & 0x00F # 0100
+
+        match first_nibble:
+            case 0x0:
+                match thrid_nibble:
+                    case 0xE:
+                        match fourth_nibble:
+                            case 0x0:
+                                self.display.clear_screen()
+                            case 0xE:
+                                returned_addr = self.memory.stack.pop()
+                                self.cpu.pc = returned_addr
+            case 0x1:
+                destination_addr = (second_nibble << 8) + (third_nibble << 4) + fourth_nibble
+                self.cpu.pc = destination_addr
+            case 0x2:
+                curr_pc = self.cpu.pc
+                self.memory.stack.append(curr_pc)
+
+                destination_addr = (second_nibble << 8) + (third_nibble << 4) + fourth_nibble
+                self.cpu.pc = destination_addr
+            case 0x3:
+                to_compare = (third_nibble << 4) + fourth_nibble
+                if self.cpu.registers[second_nibble] == to_compare:
+                    self.cpu.pc += 1
+            case 0x4:
+                to_compare = (third_nibble << 4) + fourth_nibble
+                if self.cpu.registers[second_nibble] != to_compare:
+                    self.cpu.pc += 1
+            case 0x5:
+                if self.cpu.registers[second_nibble] == self.cpu.registers[third_nibble]:
+                    self.cpu.pc += 1
+            case 0x6:
+                new_val = (third_nibble << 4) + fourth_nibble
+                self.cpu.registers[second_nibble] = new_val
+            case 0x7:
+                new_val = (third_nibble << 4) + fourth_nibble
+                self.cpu.registers[second_nibble] += new_val
+            case 0x9:
+                if self.cpu.registers[second_nibble] != self.cpu.registers[third_nibble]:
+                    self.cpu.pc += 1
+            case 0xA:
+                destination_addr = (second_nibble << 8) + (third_nibble << 4) + fourth_nibble
+                self.cpu.index = destination_addr
+            case 0xD:
+                x_coord = self.cpu.registers[second_nibble] % 64
+                y_coord = self.cpu.registers[third_nibble] % 32
+                rows = fourth_nibble
+                self.cpu.registers[0xF] = 0
+                for i in range(rows):
+                    sprite_data = self.memory.curr_memory[self.cpu.index + i]
+                    for i in range(7, 0, -1):
+                        curr_pixel = (sprite_data >> i)
+ 
+
 
 if __name__ == "__main__":
     core = Core()
-    print(core.fetch_instruction())
+    core.decode_instruction(core.fetch_instruction())
--- components/cpu.py
+++ components/cpu.py
@@ -10,20 +10,4 @@ class CPU:
         self.pc : POINTER = 0x0
 
         # defines 8 bit registers for our chip
-        self.v0 : c_uint8 = 0x0
-        self.v1 : c_uint8 = 0x0
-        self.v2 : c_uint8 = 0x0
-        self.v3 : c_uint8 = 0x0
-        self.v4 : c_uint8 = 0x0
-        self.v5 : c_uint8 = 0x0
-        self.v6 : c_uint8 = 0x0
-        self.v7 : c_uint8 = 0x0
-        self.v8 : c_uint8 = 0x0
-        self.v9 : c_uint8 = 0x0
-        self.va : c_uint8 = 0x0
-        self.vb : c_uint8 = 0x0
-        self.vc : c_uint8 = 0x0
-        self.vd : c_uint8 = 0x0
-        self.ve : c_uint8 = 0x0
-        self.vf : c_uint8 = 0x0
-
+        self.registers : List[c_uint8] = [0x0] * 16
